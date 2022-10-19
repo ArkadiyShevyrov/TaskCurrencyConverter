9278b7f7dde14c9b666e83409e48e802b383ba4e
ArkadiyShevyrov <108514147+ArkadiyShevyrov@users.noreply.github.com>
2022-10-19 12:32:58 +0300

    Add script DB

---
+++ src/main/resources/script.sql
@@ -0,0 +1,13 @@
+-- Database: CurrencyConverter
+
+-- DROP DATABASE IF EXISTS "CurrencyConverter";
+
+CREATE DATABASE "CurrencyConverter"
+    WITH
+    OWNER = root
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
