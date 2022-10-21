800b57bb3b73484a7584e38247f66a56979579aa
ArkadiyShevyrov <108514147+ArkadiyShevyrov@users.noreply.github.com>
2022-10-21 12:53:12 +0300

    Add Utils method strongToLocalDate

---
+++ src/main/java/com/example/taskcurrencyconverter/utils/Utils.java
@@ -0,0 +1,29 @@
+package com.example.taskcurrencyconverter.utils;
+
+import java.time.LocalDate;
+
+public class Utils {
+
+    public static LocalDate stringToLocalDate(String dateStr) {
+        if (dateStr == null || dateStr.equals("")) {
+            return null;
+        }
+
+        LocalDate date;
+
+        try {
+            String[] splitStr = dateStr.split("-");
+            if (splitStr.length != 3) {
+                return null;
+            }
+            int year = Integer.parseInt(splitStr[0]);
+            int mount = Integer.parseInt(splitStr[1]);
+            int day = Integer.parseInt(splitStr[2]);
+
+            date = LocalDate.of(year, mount, day);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+        return date;
+    }
+}
